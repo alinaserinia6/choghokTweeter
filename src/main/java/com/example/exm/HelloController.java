b35e7311b0429fc7343a1c9b44b6ef4544c05bce
aminak <khorrami.kh.1381@gmail.com>
2023-05-26 04:08:47 -0700

    amin do

--- src/main/java/com/example/exm/HelloController.java
+++ src/main/java/com/example/exm/HelloController.java
@@ -88,6 +88,61 @@ public class HelloController {
         stage.show();
     }
 
+    @FXML
+    private Label firstName;
+    @FXML
+    private Label lastName;
+    @FXML
+    private Label userName;
+    @FXML
+    private Label email;
+    @FXML
+    private Label password;
+    @FXML
+    private Label repeatPassword;
+    @FXML
+    private Label birthDate;
+    @FXML
+    private Label singUpError;
 
+    @FXML
+    public void singUpbuttonAction(ActionEvent e) {
+        if (firstName.getText().isEmpty()) {
+            singUpError.setText("نام نباید خالی باشد");
+            singUpError.setVisible(true);
+            return;
+        }
+        if (lastName.getText().isEmpty()) {
+            singUpError.setText("نام خانوادگی نباید خالی باشد");
+            singUpError.setVisible(true);
+            return;
+        }
+        if (userName.getText().isEmpty()) {
+            singUpError.setText("نام کاربری نباید خالی باشد");
+            singUpError.setVisible(true);
+            return;
+        }
+        if (email.getText().isEmpty()) {
+            singUpError.setText("ایمیل نباید خالی باشد");
+            singUpError.setVisible(true);
+            return;
+        }
+        if (password.getText().isEmpty()) {
+            singUpError.setText("گذرواژه نباید خالی باشد");
+            singUpError.setVisible(true);
+            return;
+        }
+        if (repeatPassword.getText().isEmpty()) {
+            singUpError.setText("تکرار گذرواژه نباید خالی باشد");
+            singUpError.setVisible(true);
+            return;
+        }
+        if (birthDate.getText().isEmpty()) {
+            singUpError.setText("تاریخ تولد نباید خالی باشد");
+            singUpError.setVisible(true);
+            return;
+        }
+
+    }
 
 }
