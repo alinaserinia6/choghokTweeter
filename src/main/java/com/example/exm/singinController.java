32cdde264d851b821420620a3bb8dae8fd8203bd
aminak <khorrami.kh.1381@gmail.com>
2023-05-26 05:40:23 -0700

    amin do signinController

---
+++ src/main/java/com/example/exm/singinController.java
@@ -0,0 +1,43 @@
+package com.example.exm;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class singinController {
+
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Label error;
+
+    @FXML
+    public void singInbuttonAction(ActionEvent e) throws IOException {
+        if (username.getText().isEmpty()) {
+            error.setText("نام کاربری نباید خالی باشد");
+            error.setVisible(true);
+            return;
+        }
+        if (password.getText().isEmpty()) {
+            error.setText("گذرواژه نباید خالی باشد");
+            error.setVisible(true);
+            return;
+        }
+        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("a3.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
--- src/main/java/com/example/exm/singincontroller.java
+++
@@ -1,4 +0,0 @@
-package com.example.exm;
-
-public class singincontroller {
-}
